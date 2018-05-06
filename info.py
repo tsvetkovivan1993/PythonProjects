d4a7e3ec3eccbde24f19a622fdc8516796354c80
Tsvetkovivan1993 <Tsvetkov.ivan1993@gmail.com>
2018-05-06 16:24:41 +0800

    new file3

---
+++ info.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+import psutil
+import os
+import psutil
+pid = os.getpid()
+py = psutil.Process(pid)
+memoryUse = py.memory_info()[0]/2.**30  # memory use in GB...I think
+print('memory use:', memoryUse)
